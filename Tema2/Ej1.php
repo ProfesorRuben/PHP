6fb28a690823e228e415d13ea876e7c20219cac4
ProfesorRuben <105635079+ProfesorRuben@users.noreply.github.com>
2022-06-12 13:01:10 +0200

    confirmar

---
+++ Tema2/Ej1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplicación</title>
+</head>
+<body>
+<i>El resultado de la multiplicación es:</i> <?php echo $_GET['num1']*$_GET['num2'] ?>
+</body>
+</html>
