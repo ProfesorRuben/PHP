89c2f4b1ba3ac25087d8fe4b2aec4e1afad73326
ProfesorRuben <105635079+ProfesorRuben@users.noreply.github.com>
2022-06-26 14:00:36 +0200

    confirmar

---
+++ Tema1/index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+
+  <?php
+  echo "<b><u>";?>Mi nombre es Romualdo.<?php echo "</u></b></br>";
+  echo "Tengo 20 años.";
+  ?>
+
+</body>
+
+</html>
