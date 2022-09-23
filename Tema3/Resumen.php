63b45fa27153a87e375ecda6cf35f0ec465ec90d
ProfesorRuben <105635079+ProfesorRuben@users.noreply.github.com>
2022-09-23 14:22:34 +0200

    resumen

--- Tema3/Resumen.php
+++ Tema3/Resumen.php
@@ -11,6 +11,17 @@
 define("PI", 3.14);
 
 echo "El valor de PI es ",PI,"</br></br>",PHP_INT_MIN;
+
+//header('location:Ejemplo2.php');
+
+//header('Refresh:3; url=Ejemplo2.php');
+
+//$_GET[‘nombre’] : (array asociativo para método get y parámetros por url ->
 ?>
+<!-- </br></br>
+<a href="Ejemplo2.php?num=1">Mi enlace</a> -->
+
+
+<!-- Para ejercicio2, 3 y 5 del boletín 4 es necesario usar isset -->
 </body>
 </html>
