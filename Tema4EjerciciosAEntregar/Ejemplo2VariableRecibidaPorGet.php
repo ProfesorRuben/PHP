541c353135f485fb4f3f7390396fd69bea33771d
ProfesorRuben <105635079+ProfesorRuben@users.noreply.github.com>
2022-09-27 23:23:25 +0200

    s

--- Tema4EjerciciosAEntregar/Ejemplo2VariableRecibidaPorGet.php
+++ Tema4EjerciciosAEntregar/Ejemplo2VariableRecibidaPorGet.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-$phpVariable = $_GET['phpVariable'];
+    <?php
+  /*   $phpVariable = $_GET['phpVariable'];
 
-echo $phpVariable;
-$phpVariable++;
+    echo $phpVariable;
+    $phpVariable++; */
 
-?>
-<form action="Ejemplo2Final.php" method="get">
-<input type="hidden" name="phpVariable" value="<?=$phpVariable?>">
-    <input type="submit" value="Envío de variable">
-</form>
+    ?>
+    <form action="Ejemplo2Final.php" method="get">
+        <input type="hidden" name="phpVariable" value="<?= $phpVariable ?>">
+        <input type="submit" value="Envío de variable">
+    </form>
 </body>
+
 </html>
